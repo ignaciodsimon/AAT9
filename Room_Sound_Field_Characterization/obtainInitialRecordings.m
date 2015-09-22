fce0cc24097ce45eeed3fca63405adbece114b19
jdomin14 <jdomin14@student.aau.dk>
2015-09-22 12:50:07 +0200

    Extracted function to perform recordings.

---
+++ Room_Sound_Field_Characterization/obtainInitialRecordings.m
@@ -0,0 +1,24 @@
+function outputData = obtainInitialRecordings()
+%% Performs the initial MLS recordings to obtain the IRs of the system.
+
+
+% First:  generate the pattern signal (MLS).
+
+% Second: create the audio signal for the six loudspeakers, multiplexing on
+%         time and with a time guard between different loudspeakers to 
+%         allow for the reverberation to decay.
+
+% Third:  include the signal on the "Ref" channel, to compensate for the
+%         delay on the acquisition system.
+
+% Fourth: start the recording on all 16 channels (15 + 1 ref) and play the
+%         previously created signal to all six loudspeakers.
+
+% Fifth:  process the recordings to obtain the IRs (deconvolution).
+
+% Sixth:  find out the associated delay time calling the necessary function
+%         and store the results for all IRs.
+
+
+    outputData = 0;
+end
