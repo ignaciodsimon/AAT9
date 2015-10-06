4398a3009cf0af6d1b085d26f1347522da1407eb
Tobias <t.vanbaarsel@gmail.com>
2015-10-06 11:59:38 +0200

    clarity computer, for room acoustics

---
+++ Arch-Env-Acoustics/clarity.m
@@ -0,0 +1,21 @@
+function [C] = clarity(IR,threshold,fs )
+%clarity computes acoustic clarity coefficient C50 or C80, depending on
+%input parameters
+%  [C] = clarity(IR,threshold )
+% inputs : Impulse Response vector, threshold [ms] (50 for C50, 80 for C80...), sampling
+% frequency [Hz]
+% output : clarity coefficient [dB]
+%
+% 2015, AAU
+
+T = threshold * fs;
+L = length(IR);
+
+num = sum(IR(1:T).^2);
+denom = sum(IR(T:L).^2);
+
+C = 10*log10(num/denom);
+
+
+end
+
